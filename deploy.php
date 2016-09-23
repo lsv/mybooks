0b1e45d092e6306577887c2d3f3cc9e268286fb1
Martin Aarhof <martin.aarhof@gmail.com>
2016-09-23 18:18:25 +0200

    php7

--- deploy.php
+++ deploy.php
@@ -5,8 +5,8 @@ set('shared_dirs', ['var/logs', 'vendor', 'var/data']);
 set('writable_dirs', ['var/cache', 'var/logs', 'var/data', 'var/sessions']);
 
 task('deploy:restart', function() {
-    run('/etc/init.d/nginx reload');
-    run('/etc/init.d/php5-fpm reload');
+    run('service nginx reload');
+    run('service php7.0-fpm reload');
 });
 
 task('deploy:clear_controllers', function () {
@@ -34,14 +34,14 @@ server('prod', 'newserver.aarhof.eu', 22)
     ->user('root')
     ->forwardAgent()
     ->stage('production')
-    ->env('deploy_path', '/var/www/books.aarhof.eu/prod')
+    ->env('deploy_path', '/ext/books.aarhof.eu/prod')
 ;
 
 server('dev', 'newserver.aarhof.eu', 22)
     ->user('root')
     ->forwardAgent()
     ->stage('development')
-    ->env('deploy_path', '/var/www/books.aarhof.eu/dev')
+    ->env('deploy_path', '/ext/books.aarhof.eu/dev')
     ->env('env_vars', 'SYMFONY_ENV=dev')
     ->env('env', 'dev')
     ->env('composer_options', 'install --verbose --prefer-dist --optimize-autoloader --no-progress --no-interaction')
